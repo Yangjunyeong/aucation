c8cea94a8d328b71bbaebffc0792fd1e07ed9254
wodnr8462 <wodnr8462@naver.com>
2023-10-26 14:17:42 +0900

    feat: jenkinsfile수정

--- BE/Aucation/src/main/java/com/example/aucation/AucationApplication.java
+++ BE/Aucation/src/main/java/com/example/aucation/AucationApplication.java
@@ -8,7 +8,7 @@ public class AucationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AucationApplication.class, args);
-		System.out.println("asdasdasd");
+		System.out.println("asdasdasdfff");
 	}
 
 }
