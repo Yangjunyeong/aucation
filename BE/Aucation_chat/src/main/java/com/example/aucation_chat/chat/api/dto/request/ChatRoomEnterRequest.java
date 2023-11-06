0ba4c8e264a35ca0257c53a36067db58e31114a4
이진서 <lalaququ@naver.com>
2023-11-06 16:10:26 +0900

    fix: 입장, 채팅 시 ChatResponse를 반환하고 RedisChatMessage에는 cached 속성 추가

--- BE/Aucation_chat/src/main/java/com/example/aucation_chat/chat/api/dto/request/ChatRoomEnterRequest.java
+++
@@ -1,5 +0,0 @@
-package com.example.aucation_chat.chat.api.dto.request;
-
-public class ChatRoomEnterRequest {
-
-}
---
+++ BE/Aucation_chat/src/main/java/com/example/aucation_chat/common/redis/pubsub/RedisPublisher.java
@@ -0,0 +1,2 @@
+package com.example.aucation_chat.common.redis.pubsub;public class RedisPublisher {
+}
