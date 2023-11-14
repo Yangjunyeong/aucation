4a90caa30fe44b629faca4139edfb6b771d76e77
wodnr8462 <wodnr8462@naver.com>
2023-11-14 10:52:54 +0900

    feat: reacution 수정

--- BE/Aucation/src/main/java/com/example/aucation/common/handler/StompHandler.java
+++ BE/Aucation/src/main/java/com/example/aucation/common/handler/StompHandler.java
@@ -7,6 +7,8 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
+import com.example.aucation.auction.api.dto.BIDRequest;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,10 +19,21 @@ public class StompHandler implements ChannelInterceptor {
 	public Message<?> preSend(Message<?> message, MessageChannel channel) {
 		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 		if (StompCommand.SUBSCRIBE == accessor.getCommand()) {
-			String auctionUUID = String.valueOf(message.getPayload());
-			String auctionUUID2 = (String) message.getHeaders().get("sessionId");
-			log.info(auctionUUID);
-			log.info(auctionUUID2);
+			log.info("accessor.getSessionId"+accessor.getSessionId());
+			String destination = accessor.getDestination();
+			log.info("destination"+destination);
+			String[] destinationParts = destination.split("/");
+			String auctionUUID = destinationParts[destinationParts.length - 1];
+			log.info("AuctionUUID: " + auctionUUID);
+
+			log.info("===========================================");
+
+			String a = (String)message.getHeaders().get("simpDestination");
+			String b = (String)message.getHeaders().get("simpSessionId");
+
+			log.info("이제진짜인가?"+a);
+			log.info("이제진짜인가????"+b);
+
 		}else if(StompCommand.DISCONNECT == accessor.getCommand()){
 			String auctionUUID = String.valueOf(message.getPayload());
 			log.info(auctionUUID);
