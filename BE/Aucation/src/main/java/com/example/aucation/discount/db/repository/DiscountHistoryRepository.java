f210be01b133efc0dc0dae9c2039c28b88694015
wodnr8462 <wodnr8462@naver.com>
2023-11-16 16:20:09 +0900

    feat: buy

--- BE/Aucation/src/main/java/com/example/aucation/discount/db/repository/DiscountHistoryRepository.java
+++ BE/Aucation/src/main/java/com/example/aucation/discount/db/repository/DiscountHistoryRepository.java
@@ -23,6 +23,7 @@ public interface DiscountHistoryRepository extends JpaRepository<DiscountHistory
 
 	boolean existsDiscountHistoryByDiscount(Discount discount);
 
+	@Modifying
 	@Query("SELECT disHistory FROM DiscountHistory disHistory WHERE disHistory.discount.id = :discountId AND disHistory.customer.id = :memberId")
-	DiscountHistory findByDiscountIdAndMemberId(Long discountId, Long memberId);
+	Optional<DiscountHistory> findByDiscountIdAndMemberId(Long memberId, Long discountId);
 }
